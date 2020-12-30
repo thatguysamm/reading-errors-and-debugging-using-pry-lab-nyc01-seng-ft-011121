c1df3fb82e886f82110eaa7c66f322eeac95885c
friendly-format-0074 <samuelagasi@gmail.com>
2020-12-30 04:41:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,11 +14,9 @@ require 'pry'
 def selection(num)
   if num == 1
   "YUM YUM MUNCH MUNCH MUNCH"
-  binding.pry
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+elsif num == 2
+  "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
   puts 'hello'
 end
 
